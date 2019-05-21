d23edb027af0eace3eed9a2f80b53812545af473
Michelle Moon <michellemoon236@gmail.com>
2019-05-21 03:04:29 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -9,10 +9,9 @@ require 'bundler/setup'
 Bundler.require
 
 
+require_relative "../artist.rb"
+
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/artists.sqlite"
 )
-
-
-require_relative "../artist.rb"
